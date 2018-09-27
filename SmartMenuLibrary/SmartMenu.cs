6c0333e55b0b33e7fa695698243006514bb43745
Niclas Holmquist Jensen <nicl2445@edu.eal.dk>
2018-09-27 10:58:42 +0200

    writeline lavet

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -11,11 +11,12 @@ namespace SmartMenuLibrary
         public void LoadMenu(string path)
         {
             //splitter et array op i to lige store arrays
+            /*
             string[] testarray = { "bla", "bla1", "bla2", "bla3" };
 
             string[] danskArray = testarray.Take(testarray.Length / 2).ToArray();
             string[] engelskArray = testarray.Skip(testarray.Length / 2).ToArray();
-
+            */
 
             string[] heleTxt = System.IO.File.ReadAllLines(@"c:..\..\" + path + "");
             string[] danskTxt = heleTxt.Take(heleTxt.Length / 2).ToArray();
@@ -26,7 +27,20 @@ namespace SmartMenuLibrary
         }
         public void Activate()
         {
-            // Implement ...
+            Console.WriteLine("Vælg sprog - Choose language: \n Tryk D for Dansk \n Press E for English");
+            string sprog = Console.ReadLine();
+            if (sprog == "d")
+            {
+                Console.WriteLine("Du har valgt dansk\n" + danskTxt); /*vi skal have lavet så man kan hente de to string fra loadmenu metoden*/
+            }
+            else if (sprog == "e")
+            {
+                Console.WriteLine("You have chosen English\n" + englishTxt); /*vi skal have lavet så man kan hente de to string fra loadmenu metoden*/
+            }
+            else
+            {
+                Console.WriteLine("Du har ikke valgt et gyldigt sprog - You have not chosen a viable language");
+            }
         }
     }
 }

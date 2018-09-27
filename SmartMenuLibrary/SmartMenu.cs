3e4f34e2026510046d637236e956dcf8b6dfdbea
Pete59f1 <pete59f1@edu.eal.dk>
2018-09-27 10:01:32 +0200

    Tilføjet nogle for loops

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -10,7 +10,27 @@ namespace SmartMenuLibrary
     {
         public void LoadMenu(string path)
         {
-            // Implement ...
+            string[] heleTxt = System.IO.File.ReadAllLines(@"c:..\..\" + path + "");
+            string[] danskTxt = new string[heleTxt.Length / 2];
+            string[] englishTxt = new string[heleTxt.Length / 2];
+
+            for(int i = 0; i < heleTxt.Length; i++)
+            {
+                if(heleTxt[i] == "Dansk:")
+                {
+                    for(int j = 0; j < danskTxt.Length; j++)
+                    {
+                        danskTxt[j] = heleTxt[i];
+                    }
+                }
+                if(heleTxt[i] == "English:")
+                {
+                    for(int l = 0; l < englishTxt.Length; l++)
+                    {
+                        englishTxt[l] = heleTxt[i];
+                    }
+                }
+            }
         }
         public void Activate()
         {

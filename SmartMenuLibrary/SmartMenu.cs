1fad891db8a5e2ec16494cb30545e7f8b3362f7c
Pete59f1 <pete59f1@edu.eal.dk>
2018-10-01 14:10:41 +0200

    Tilføjet Do While. Men det virker ikke helt endnu!

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -11,6 +11,7 @@ namespace SmartMenuLibrary
         string[] danskTxt;
         string[] englishTxt;
         int[] callID;
+        string userInput = null, userInputSprog = null;
         public void LoadMenu(string path)
         {
 
@@ -36,9 +37,9 @@ namespace SmartMenuLibrary
 
         public void Activate()
         {
-            label1:
+            //label1:
             Console.WriteLine("Vælg sprog - Choose language: \n Tryk D for Dansk \n Press E for English");
-            string sprog = Console.ReadLine().ToLower();
+            /*string sprog = Console.ReadLine().ToLower();
 
             if (sprog == "d")
             {
@@ -89,7 +90,7 @@ namespace SmartMenuLibrary
             {
                 Console.WriteLine("Afslutter program - Stopping program");
             }
-            else
+            /*else
             {
                 if (sprog == "d")
                 {
@@ -101,7 +102,73 @@ namespace SmartMenuLibrary
                     Console.WriteLine("You have not selected a valid menu point. Try again");
                     goto label2;
                 }
+            }*/
+
+            do
+            {
+                userInputSprog = Console.ReadLine().ToLower();
+
+                if (userInputSprog == "d")
+                {
+                    Console.WriteLine("Du har valgt dansk\n");
+                    for (int i = 2; i < danskTxt.Length; i++)
+                    {
+                        Console.WriteLine(danskTxt[i]);
+                    }
+                    Console.WriteLine("\n" + "(" + danskTxt[1] + ")");
+                }
+                else if (userInputSprog == "e")
+                {
+                    Console.WriteLine("You have chosen English\n");
+                    for (int i = 2; i < englishTxt.Length; i++)
+                    {
+                        Console.WriteLine(englishTxt[i]);
+                    }
+                    Console.WriteLine("\n" + "(" + englishTxt[1] + ")");
+                }
+                else
+                {
+                    Console.WriteLine("Du har ikke valgt et gyldigt sprog - You have not chosen a viable language");
+                }
             }
+            while (userInputSprog != "d" || userInputSprog != "e");
+
+            do
+            {
+                userInput = Console.ReadLine();
+                if (userInput == "1")
+                {
+                    Console.WriteLine(FunctionLibrary.Functions.DoThis());
+                }
+                else if (userInput == "2")
+                {
+                    Console.WriteLine(FunctionLibrary.Functions.DoThat());
+                }
+                else if (userInput == "3")
+                {
+                    Console.WriteLine(FunctionLibrary.Functions.DoSomething(userInput));
+                }
+                else if (userInput == "4")
+                {
+                    Console.WriteLine(FunctionLibrary.Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+                }
+                else if (userInput == "0")
+                {
+                    Console.WriteLine("Afslutter program - Stopping program");
+                }
+                else
+                {
+                    if (userInputSprog == "d")
+                    {
+                        Console.WriteLine("Du har ikke valgt et gyldigt menu punkt. Prøv igen");
+                    }
+                    else if (userInputSprog == "e")
+                    {
+                        Console.WriteLine("You have not selected a valid menu point. Try again");
+                    }
+                }
+            }
+            while (userInput != "0");
         }
     }
 }

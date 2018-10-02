049f691220622ee00ccbedaa581cbab06ef88d25
Pete59f1 <pete59f1@edu.eal.dk>
2018-10-02 11:58:50 +0200

    Rytter op

--- SmartMenuLibrary/SmartMenu.cs
+++ SmartMenuLibrary/SmartMenu.cs
@@ -15,13 +15,11 @@ namespace SmartMenuLibrary
         public void LoadMenu(string path)
         {
 
-
             string[] heleTxt = System.IO.File.ReadAllLines(@"..\..\" + path + "");
             danskTxt = heleTxt.Take(heleTxt.Length / 2).ToArray();
             englishTxt = heleTxt.Skip(heleTxt.Length / 2).ToArray();
             callID = new int[danskTxt.Length - 2];
             
-
             for(int i = 0; i < danskTxt.Length; i++)
             {
                 int j = 0;
@@ -37,72 +35,7 @@ namespace SmartMenuLibrary
 
         public void Activate()
         {
-            //label1:
             Console.WriteLine("Vælg sprog - Choose language: \n Tryk D for Dansk \n Press E for English");
-            /*string sprog = Console.ReadLine().ToLower();
-
-            if (sprog == "d")
-            {
-                Console.WriteLine("Du har valgt dansk\n"); 
-                for(int i = 2; i < danskTxt.Length; i++)
-                {
-                    Console.WriteLine(danskTxt[i]);
-                }
-                Console.WriteLine("\n" + "(" + danskTxt[1] + ")");
-            }
-            else if (sprog == "e")
-            {
-                Console.WriteLine("You have chosen English\n"); 
-                for (int i = 2; i < englishTxt.Length; i++)
-                {
-                    Console.WriteLine(englishTxt[i]);
-                }
-                Console.WriteLine("\n" + "(" + englishTxt[1] + ")");
-            }
-            else
-            {
-                Console.WriteLine("Du har ikke valgt et gyldigt sprog - You have not chosen a viable language");
-                goto label1;
-            }
-            label2:
-            string menupunkt = Console.ReadLine();
-            if (menupunkt == "1")
-            {
-                Console.WriteLine(FunctionLibrary.Functions.DoThis());
-                goto label2;
-            }
-            else if(menupunkt == "2")
-            {
-                Console.WriteLine(FunctionLibrary.Functions.DoThat());
-                goto label2;
-            }
-            else if (menupunkt == "3")
-            {
-                Console.WriteLine(FunctionLibrary.Functions.DoSomething(menupunkt));
-                goto label2;
-            }
-            else if (menupunkt == "4")
-            {
-                Console.WriteLine(FunctionLibrary.Functions.GetTheAnswerToLifeTheUniverseAndEverything());
-                goto label2;
-            }
-            else if (menupunkt == "0")
-            {
-                Console.WriteLine("Afslutter program - Stopping program");
-            }
-            /*else
-            {
-                if (sprog == "d")
-                {
-                    Console.WriteLine("Du har ikke valgt et gyldigt menu punkt. Prøv igen");
-                    goto label2;
-                }
-                else if (sprog == "e")
-                {
-                    Console.WriteLine("You have not selected a valid menu point. Try again");
-                    goto label2;
-                }
-            }*/
 
             do
             {
